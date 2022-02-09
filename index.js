bced09b988e63f29dbf0ff2e569f4bca25002c68
sabrinaaquino <sabrina.aquino@hotmail.com>
2022-02-09 18:37:10 -0300

    add .html files

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/', function(req,res){
+
+    res.sendFile(path.join(__dirname+'/index.html'));
+
+});
+
+router.get('/sobre', function(req,res){
+
+    res.sendFile(path.join(__dirname+'/about.html'));
+
+});
+
+app.use('/',router);
+//use standard port or port 3000
+app.listen(process.env.port || 3000);
+
+console.log("Server rodando");
+
+
+/*
+
+Creating only the server (without html) 
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -13,3 +44,4 @@ const server = http.createServer((req,res) => {
 server.listen(port,hostname, () => {
     console.log("servidor rodando!");
 })
+*/
